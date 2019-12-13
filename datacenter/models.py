8b2a19ae1b1bd5a29624ce920bb46e91d8fde7f4
Andrey Usov <nkt.andr@gmail.com>
2019-12-13 18:40:06 +0300

    fix watching-storage

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Passcard(models.Model):
@@ -23,5 +24,29 @@ class Visit(models.Model):
         return "{user} entered at {entered} {leaved}".format(
             user=self.passcard.owner_name,
             entered=self.entered_at,
-            leaved= "leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
+            leaved="leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
         )
+
+    def get_duration(self):
+        duration = (self.get_leaved_at() - self.entered_at).seconds
+        return duration
+
+    def format_duration(self, duration):
+        hours = duration // 3600
+        minutes = (duration % 3600) // 60
+        return f'{hours}ч {minutes}мин'
+
+    def get_leaved_at(self):
+        if not self.leaved_at:
+            leaved_at = timezone.now()
+        else:
+            leaved_at = self.leaved_at
+        return leaved_at
+
+    def is_long(self, minutes=60):
+        visit_time_second = (self.get_leaved_at() - self.entered_at).seconds
+        visit_time_minutes = visit_time_second // 60
+        if visit_time_minutes > minutes:
+            return True
+        else:
+            return False
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,15 +4,18 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = list()
 
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
-    ]
+    for visit in visits:
+        duration = visit.get_duration()
+        non_closed_visits.append({
+            "who_entered": visit.passcard.owner_name,
+            "entered_at": visit.entered_at,
+            "duration": visit.format_duration(duration),
+            "is_strange": visit.is_long()
+        })
+
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
