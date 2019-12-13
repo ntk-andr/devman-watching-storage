6868da1b9f4dac88c2fdaf8fde4b2141e1dd6443
Andrey Usov <nkt.andr@gmail.com>
2019-12-13 16:43:27 +0300

    work in cli

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ django.setup()
 
 from datacenter.models import Passcard, Visit
 
-
 if __name__ == "__main__":
     # Программируем здесь
+    passcards = Passcard.objects.all()
+
+    print(passcards)
+
+    passcard = passcards[0]
+    print('owner_name:', passcard.owner_name)
+    print('passcode:', passcard.passcode)
+    print('created_at:', passcard.created_at)
+    print('is_active:', passcard.is_active)
+
     print('Количество пропусков:', Passcard.objects.count())
+
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print('Активных пропусков:', len(active_passcards))
